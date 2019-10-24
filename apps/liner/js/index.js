fefa67db4ffe9b687f7ea95cd4d899f124200258
bjoerndot <bjoerndot@gmail.com>
2019-10-24 23:02:26 +0200

    Interface update & more buttons

--- apps/liner/js/index.js
+++ apps/liner/js/index.js
@@ -6,100 +6,143 @@
  * http://link-to-your-git-repo.com
  */
 
+/*jshint esversion: 6 */
 'use strict';
 
-console.log("I'M here")
-
 // line-height as mm
-var currentLineHeight = 15
+let currentLineHeight = 15;
+// upper length as percentage
+let currentUpperLength = 40;
+// line distance as mm
+let currentLineDistance = 15/2;
+// all values in an array for easy iterating
+let markers = [currentLineHeight, currentUpperLength, currentLineDistance];
 
 function getPage(){
-    return document.getElementsByClassName("liner_canvas")[0]
-};
+    return document.getElementsByClassName("liner_canvas")[0];
+}
 
 function setPageHeight(){
-    var pageElement = getPage();
-    var pageWidth = pageElement.clientWidth
-    // DIN A4 is pageWidth * squrt(2)
-    var pageHeight = pageWidth * Math.sqrt(2)
-    pageElement.style.height = `${pageHeight}px`
+    // calculate the pageHeight based on the DIN-Format and set it in client
+    // DIN A is pageWidth * squrt(2)
+    let pageElement = getPage();
+    let pageHeight = pageElement.clientWidth * Math.sqrt(2);
+    pageElement.style.height = `${pageHeight}px`;
 }
 
-function calculateLineHeight(pageHeight, lineHeightValue){
-    return pageHeight * (lineHeightValue/297)
+function mmToPixels(pageHeight, value){
+    // returns the pixels of the page * the percentage of the value on a DIN A 4 sheet
+    return pageHeight * (value/297);
 }
 
-function getNumLines(pageHeight, lineHeight){
-    return Math.floor(pageHeight / (lineHeight+lineHeight/2))
+function getNumLines(pageHeight, lineHeight, lineDistance){
+    // give the height of the page only a certain amount of lines + distances fit onto the page
+    return Math.floor(pageHeight / (lineHeight + lineDistance));
 }
 
-function getLineValues(pageElement, lineHeightValue){
-    var pageHeight = pageElement.clientHeight;
-    var lineHeight = calculateLineHeight(pageHeight, lineHeightValue)
-    var numLines = getNumLines(pageHeight, lineHeight);
-    return {lineHeight, numLines}
+function getLineValues(pageElement, lineHeightValue, lineDistanceValue){
+    let pageHeight = pageElement.clientHeight;
+    // calculating line height in pixels based on given ratios
+    let lineHeight = mmToPixels(pageHeight, lineHeightValue);
+    // calculating line distance in pixels based on given ratios
+    let lineDistance = mmToPixels(pageHeight, lineDistanceValue);
+    let numLines = getNumLines(pageHeight, lineHeight, lineDistance);
+    return {lineHeight, lineDistance, numLines};
 }
 
 function setSizeMarker(){
-    console.log(currentLineHeight)
-    var sizeMarker = document.getElementsByClassName("settings_label")[0];
-    sizeMarker.innerHTML = sizeMarker.innerHTML.replace(/\d{2}/, currentLineHeight)
-};
+    let labels = document.getElementsByClassName("settings_label");
+    for(let idx = 0; idx < labels.length; idx++){
+        labels[idx].innerHTML = labels[idx].innerHTML.replace(/\d{1,2}\.?\d{0,1}/, markers[idx]);
+    }
+}
 
-function renderLines(numLines, lineHeight, pageElement){
+function renderLines(numLines, lineHeight, lineDistance, pageElement){
     pageElement.innerHTML = "";
     setSizeMarker();
-    var i;
+    let i;
     for(i = 0; i<numLines; i++){
         // create divider-element to easier see the lines
-        var divider = document.createElement("div")
-        divider.classList.add("liner_divider")
-        divider.style.height = `${lineHeight/2}px`
+        const divider = document.createElement("div");
+        divider.classList.add("liner_divider");
+        divider.style.height = `${lineDistance}px`;
         // create line
-        var line = document.createElement("div")
-        line.classList.add("liner_line")
-        line.style.height = `${lineHeight}px`
+        const line = document.createElement("div");
+        line.classList.add("liner_line");
+        line.style.height = `${lineHeight}px`;
+        const line_middle = document.createElement("div");
+        line_middle.classList.add("liner_line--middle");
+        line_middle.style.height = `${currentUpperLength}%`;
         pageElement.appendChild(divider);
         pageElement.appendChild(line);
+        line.appendChild(line_middle);
     }
 }
 
-function setLineHeigt(){
+function setLineValues(){
     //base line-height = 15mm, line-margin: line-height/2
     //297mm  --> ratio = 15mm/297mm
-    var pageElement = getPage();
-    var {lineHeight, numLines} = getLineValues(pageElement, currentLineHeight)
-    renderLines(numLines, lineHeight, pageElement)
-
-};
+    const pageElement = getPage();
+    const {lineHeight, lineDistance, numLines} = getLineValues(pageElement, currentLineHeight, currentLineDistance);
+    renderLines(numLines, lineHeight, lineDistance, pageElement);
+}
 
 function initializingSizes(){
+    // page must be set as the width of the page is relational to the width of the client
     setPageHeight();
-    setLineHeigt();
+    setLineValues();
 }
 
-function changeLineHeight(){
-    var pageElement = getPage();
-    const {lineHeight, numLines} = getLineValues(pageElement, currentLineHeight)
-    renderLines(numLines, lineHeight, pageElement)
-};
+
+function updateSizes(sizes){
+
+    currentLineHeight = sizes.lineHeight;
+    currentUpperLength = sizes.upperLength;
+    currentLineDistance = sizes.lineDistance;
+    markers = [currentLineHeight, currentUpperLength, currentLineDistance];
+}
 
 function handleLineClick(e){
-    var direction = e.target.dataset.dir
-    if(direction === "+"){
-        currentLineHeight++;
-        changeLineHeight();   
-    } else {
-        currentLineHeight--;  
-        changeLineHeight();  
+    let button = e.target.closest("button");
+    let direction = button.dataset.dir;
+    let lineHeight = currentLineHeight;
+    let upperLength = currentUpperLength;
+    let lineDistance = currentLineDistance;
+
+    if (button.dataset.module === "line-height"){
+        if(direction === "+"){
+            lineHeight = Math.min(currentLineHeight + 1, 297);
+        } else {
+            lineHeight = Math.max(currentLineHeight - 1, 0); 
+        }
     }
+    else if (button.dataset.module === "upper-length"){
+        if(direction === "+"){
+            upperLength = Math.min(currentUpperLength + 10, 100);
+        } else {
+            upperLength = Math.max(currentUpperLength - 10, 0);
+        }
+    }else{
+        if(direction === "+"){
+            // line distance may never become higher or lower than the page itself
+            lineDistance = Math.min(currentLineDistance + 0.5, 297);
+        } else {
+            lineDistance = Math.max(currentLineDistance - 0.5, 0);          
+        }
+    }
+    updateSizes({
+        lineHeight,
+        upperLength,
+        lineDistance,
+    });
+    setLineValues();  
+
 }
 
-var buttons = document.querySelectorAll('[data-module]');
-var actions = []
-console.log(buttons)
+let buttons = document.querySelectorAll('[data-module]');
+
 buttons.forEach((button) => {
-    button.addEventListener("click", handleLineClick)
+    button.addEventListener("click", handleLineClick);
 });
 
-window.addEventListener("load", initializingSizes)
+window.addEventListener("load", initializingSizes);
---
+++ apps/liner/readme.md
@@ -0,0 +1,7 @@
+#Liner
+## Latern app for calligraphy and writing in general
+
+Project for google lantern that projects lines onto a sheet of paper. 
+
+Line height, line distance and the ascender height can be changed by tapping the respective + and - buttons.
+Dimensions follow the DIN A format and mm are based on DIN A 4
